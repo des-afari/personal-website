789e1ba924ea87dcad16e8051431205300e70589
unknown <afaridesmond@gmail.com>
2023-12-22 16:13:08 +0000

    contact page

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import About from "./components/About"
+import Contact from "./components/Contact"
 import Education from "./components/Education"
 import Index from "./components/Index"
 import Projects from "./components/Projects"
@@ -13,6 +14,7 @@ function App() {
         <Projects />
         <Education />
         <Technologies />
+        <Contact />
       </div>
     </div>
   )
--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,8 +1,89 @@
-import React from 'react'
+import { FC } from 'react'
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import * as z from "zod"
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { Textarea } from './ui/textarea'
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
 
-const Contact = () => {
+const formSchema = z.object({
+  email: z
+  .string()
+  .email({
+    message: "Please enter a valid email address.",
+  }),
+  message: z
+    .string()
+    .min(10, {
+      message: "Message must be at least 10 characters.",
+    })
+    .max(160, {
+      message: "Message must not be longer than 160 characters.",
+    }),
+})
+
+const Contact: FC = () => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      message: ""
+    },
+  })
+ 
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values)
+  }
+
   return (
-    <div>Contact</div>
+    <section className='pt-24'>
+      <header className='h-12 flex items-center sticky top-0 backdrop-blur-sm mb-4'>
+        <h4 className='text-sm tracking-widest font-bold text-white'>GET IN TOUCH</h4>
+      </header>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input placeholder="Enter your email address" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+          control={form.control}
+          name="message"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Message</FormLabel>
+              <FormControl>
+                <Textarea
+                  placeholder="Type your message here"
+                  className="h-32 resize-none"
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+          />
+          <Button type="submit" className='w-full' variant={'secondary'}>Submit</Button>
+        </form>
+      </Form>
+    </section>
   )
 }
 
